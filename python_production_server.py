bb041b53b10c6dfb5f7d1a8ea3b326ee5f25c4e5
Eivind Lie Andreassen <ela4991@gmail.com>
2018-06-28 15:43:38 +0200

    Add basic sync request implementation

--- python_production_server.py
+++ python_production_server.py
@@ -2,6 +2,7 @@ import flask
 import string
 import random
 import sys
+import inspect
 
 _archives = {}
 _type_map = {
@@ -18,12 +19,12 @@ def register_function(archive, func):
     if archive not in _archives:
         _archives[archive] = {
             'uuid': archive + '_' + ''.join(random.choices(string.ascii_uppercase + string.digits, k=32)),
-            'functions': []
+            'functions': {}
         }
-    _archives[archive]['functions'].append(func)
+    _archives[archive]['functions'][func.__name__] = func
 
 
-@_app.route('/api/discovery')
+@_app.route('/api/discovery', methods=['GET'])
 def _discovery():
     response = {
         'discoverySchemaVersion': '1.0.0',
@@ -40,11 +41,11 @@ def _discovery():
             'matlabRuntimeVersion': py_version
         }
 
-        for func in archive['functions']:
+        for func_name, func in archive['functions'].items():
             assert len(func.__annotations__), 'All functions must be annotated!'
             assert 'return' in func.__annotations__, 'Return type must be annotated!'
 
-            arch_response['functions'][func.__name__] = {
+            arch_response['functions'][func_name] = {
                 'signatures': [{
                     'inputs': [],
                     'outputs': [
@@ -57,19 +58,44 @@ def _discovery():
                 }]
             }
 
-            for name, typ in func.__annotations__.items():
-                if name == 'return':
-                    continue
-
+            for par_name in list(inspect.signature(func).parameters):
                 arch_response['functions'][func.__name__]['signatures'][0]['inputs'].append({
                     'mwsize': [],
-                    'mwtype': _type_map[typ],
-                    'name': name
+                    'mwtype': _type_map[func.__annotations__[par_name]],
+                    'name': par_name
                 })
 
         response['archives'][archive_key] = arch_response
 
     return flask.jsonify(response)
+
+
+def _sync_request(archive_name, function_name, request):
+    params = flask.json.loads(request.form['rhs'])
+    func = _archives[archive_name]['functions'][function_name]
+
+    for i, par_name in enumerate(list(inspect.signature(func).parameters)):
+        params[i] = func.__annotations__[par_name](params[i])
+
+    result = func(*params)
+
+    return flask.jsonify({'lhs': result})
+
+
+def _async_request(archive_name, function_name, request):
+    func = _archives[archive_name]['functions'][function_name]
+    # TODO Implement asynchronous requests
+    return 'Asynchronous requests not yet implemented'
+
+
+@_app.route('/<archive_name>/<function_name>', methods=['POST'])
+def _call_function(archive_name, function_name):
+    mode = flask.request.args.get('mode', False)
+    if mode and mode == 'async':
+        return _async_request(archive_name, function_name, flask.request)
+    else:
+        return _sync_request(archive_name, function_name, flask.request)
+
 
 
 def run():
