e2e174c264be217dbc8dbde93ca3ec95a80af75a
Eivind Lie Andreassen <ela4991@gmail.com>
2018-07-03 11:11:37 +0200

    Add multiply test function

--- test.py
+++ test.py
@@ -2,16 +2,10 @@ import python_production_server
 import numpy as np
 
 
-def my_test_function(in1: np.double, in2: np.ndarray(shape=(1, 2), dtype=np.integer), in3:float) -> (str, int):
-    """
-    My docstring
-    :param in1: Input paramater
-    :return: Test string
-    """
-    return 'Input was: ' + str(in1), in1
+def multiply(x: np.double, y: np.double) -> np.double:
+    return x * y
 
 
 if __name__ == '__main__':
-    python_production_server.register_function('test_archive', my_test_function)
-
+    python_production_server.register_function('basic_arithmetics', multiply)
     python_production_server.run(port=8080)
