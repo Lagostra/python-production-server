5063898d53f6e9d6875ab8400a640670ee9e7ed5
Eivind Lie Andreassen <ela4991@gmail.com>
2018-07-05 08:32:50 +0200

    Add 0 as indication of unknown dimension

--- python_production_server.py
+++ python_production_server.py
@@ -179,9 +179,13 @@ def register_function(archive, func):
 def _evaluate_type(annotation):
     if type(annotation) == np.ndarray:
         typ = _type_map[annotation.dtype.__str__()]
-        size = annotation.shape
-        if len(size) == 1:
-            size = (size[0], 'X')
+        if len(annotation.shape) == 1:
+            # Assume that a 1D array of unknown length is wanted
+            size = (annotation.shape[0], 'X')
+        else:
+            size = []
+            for d in annotation.shape:
+                size.append(d if d != 0 else 'X')
     else:
         typ = _type_map[annotation]
         size = [1, 1]
--- test.py
+++ test.py
@@ -11,7 +11,12 @@ def dot_product(x: np.ndarray(shape=(1,), dtype=np.double), y: np.ndarray(shape=
     return x * y
 
 
+def transpose(x: np.ndarray(shape=(0, 0), dtype=np.double)) -> np.ndarray(shape=(0, 0), dtype=np.double):
+    return x.T
+
+
 if __name__ == '__main__':
     python_production_server.register_function('basic_arithmetics', multiply)
     python_production_server.register_function('basic_arithmetics', dot_product)
+    python_production_server.register_function('basic_arithmetics', transpose)
     python_production_server.run(port=8080)
