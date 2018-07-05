d477ea0b5a833346d17fb337127649a1d0a8a0ce
Eivind Lie Andreassen <ela4991@gmail.com>
2018-07-05 13:51:13 +0200

    Prettify

--- autoload/basic_arithmetics.py
+++ autoload/basic_arithmetics.py
@@ -8,6 +8,7 @@ def multiply(x: np.double, y: np.double) -> np.double:
 def divide(x: np.double, y: np.double) -> np.double:
     return x / y
 
+
 def dot_product(x: np.ndarray(shape=(1,), dtype=np.double), y: np.ndarray(shape=(1,), dtype=np.double)) \
                                                             -> np.double:
     return x * y
