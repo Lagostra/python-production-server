5e9fda714790e6b511ec7f59ce52d72df96d79ee
Eivind Lie Andreassen <ela4991@gmail.com>
2018-08-09 09:50:42 +0200

    Fix dimensions of dot_product in basic_arithmetics example

--- autoload/basic_arithmetics.py
+++ autoload/basic_arithmetics.py
@@ -9,7 +9,7 @@ def divide(x: np.double, y: np.double) -> np.double:
     return x / y
 
 
-def dot_product(x: np.ndarray(shape=(1,), dtype=np.double), y: np.ndarray(shape=(1,), dtype=np.double)) \
+def dot_product(x: np.ndarray(shape=(1,0), dtype=np.double), y: np.ndarray(shape=(1,0), dtype=np.double)) \
                                                             -> np.double:
     return x * y
 
